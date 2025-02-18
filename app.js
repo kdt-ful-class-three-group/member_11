f03389dbb183e090c08cb180838f1855f35023c8
전선일 <jeonsunil1225@gmail.com>
2025-02-18 18:25:01 +0900

    post데이터를 받아와서 출력.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const qs = require('querystring');
 
 const server = http.createServer(function(req, res) {
   console.log(req.url);
@@ -13,6 +14,29 @@ const server = http.createServer(function(req, res) {
       res.writeHead(404, {'content-type': 'text/html'});
       res.write('<h1>404 Not Found</h1>');
       res.end();
+    }
+  }
+  if(req.method === 'POST') {
+    if(req.url === '/data') {
+
+      req.on('data', function(data) {
+        // on = addEventListener 같은것.
+        console.log(qs.stringify(data));
+        // func.getData(data);
+        fs.writeFileSync('data.txt', data.toString());
+      });
+      req.on('end', function(){
+        // on('end', ) = 이벤트를 끝낸다는 뜻.
+        console.log("응답 잘 받음");
+        const data = fs.readFileSync('data.txt').toString();
+
+        console.log(data);
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`<h1>${data.split('=')[1]}</h1>`);
+        res.end();
+      });
     }
   }
 });
