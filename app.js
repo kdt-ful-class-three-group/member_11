ad44d0f70660ed6689722a63038f3e88147cb3fd
전선일 <jeonsunil1225@gmail.com>
2025-02-19 15:30:29 +0900

    엔드포인트 설정 및 간단한 style.css생성, html에 css연결.

--- app.js
+++ app.js
@@ -2,11 +2,22 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer(function(req, res) {
-
+  console.log(req.url)
+  if(req.method === 'GET') {
+    if(req.url === '/') {
+      res.writeHead(200, {'content-type': 'text/html; charset=utf-8'});
+      res.write(fs.readFileSync('index.html'));
+      res.end();
+    }
+    if(req.url === '/style.css') {
+      res.writeHead(200, {'content-type': 'text/css; charset=utf-8'});
+      res.write(fs.readFileSync('style.css'));
+      res.end();
+    }
+  }
 });
 
-
-const PORT = 3000;
+const PORT = 5500;
 server.listen(PORT, function() {
   console.log(`서버가 http://localhost:${PORT} 에서 실행중입니다.`);
 });
